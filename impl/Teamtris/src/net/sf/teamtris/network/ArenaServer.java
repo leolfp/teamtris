0d4803950a7984d307bccf9cac8e9d3a381e29a4
leolfp <leolfp@22ab3def-6142-0410-af02-65c79cd6b76c>
2007-12-31 17:57:56 +0000

    Logging support.

--- impl/Teamtris/src/net/sf/teamtris/network/ArenaServer.java
+++ impl/Teamtris/src/net/sf/teamtris/network/ArenaServer.java
@@ -1,12 +1,42 @@
 package net.sf.teamtris.network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.sf.teamtris.arena.ServingArena;
+
 /**
- * The class that remotely serves an serving arena for network games. 
+ * The class that remotely serves a serving arena for network games. 
  * @author Leonardo
  * @version 1.0
  * @created 31-dez-2007 14:01:01
  */
-public class ArenaServer {
+public class ArenaServer implements Runnable {
+	private static final Log log = LogFactory.getLog(ArenaServer.class);
+	
+	private final ServingArena servedArena;	
 
+	/**
+	 * The default constructor for an arena server.
+	 * @param servedArena The arena to be served on the network.
+	 */
+	public ArenaServer(ServingArena servedArena) {
+		this.servedArena = servedArena;
+	}
 
+	@Override
+	public void run() {
+		ServerSocket server;
+		try {
+			server = new ServerSocket(RemoteArena.REMOTE_PORT);
+		} catch (IOException e) {
+			log.fatal("Fail to initialize server socket.", e);
+			return;
+		}
+		
+		
+	}
 }
--- impl/Teamtris/src/net/sf/teamtris/network/RemoteArena.java
+++ impl/Teamtris/src/net/sf/teamtris/network/RemoteArena.java
@@ -14,6 +14,7 @@ import net.sf.teamtris.arena.Player;
  * @created 31-dez-2007 14:01:15
  */
 public class RemoteArena implements Arena {
+	public static final int REMOTE_PORT = 1234;	
 
 	@Override
 	public Game getGame(Player player) {
