e5fd2f2884a304a1f24a40c5c174ce3e37ff2a74
leolfp <leolfp@22ab3def-6142-0410-af02-65c79cd6b76c>
2008-01-06 17:33:24 +0000

    Piece stream support.

--- trunk/impl/Teamtris/src/net/sf/teamtris/piece/Piece.java
+++ trunk/impl/Teamtris/src/net/sf/teamtris/piece/Piece.java
@@ -1,5 +1,10 @@
 package net.sf.teamtris.piece;
 
+import java.text.ParseException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * A bean that describes a tetris piece.
  * @author Leonardo
@@ -7,5 +12,61 @@ package net.sf.teamtris.piece;
  * @created 31-dez-2007 14:02:46
  */
 public class Piece {
+	private static final Log log = LogFactory.getLog(Piece.class);
+	
+	/**
+	 * The internal block representation for this piece.
+	 * Indexed first by lines and second by columns.
+	 */
+	private final boolean[][] blocks;
+	
+	private final String color;
+	
+	/**
+	 * Constructs a piece using its string representation, like
+	 * <code>' ##:## '</code> for the <code>S</code> piece.
+	 * @param pieceSerialization The piece string representation.
+	 * @param color The color string, as hexa RRGGBB.
+	 * @throws ParseException If the piece serialization or the color is invalid.
+	 */
+	public Piece(String pieceSerialization, String color) throws ParseException{
+		String[] lines = pieceSerialization.split(":");
+		blocks = new boolean[lines.length][];
+		for (int l = 0; l < lines.length; l++) {
+			blocks[l] = new boolean[lines[l].length()];
+			for(int c = 0; c < lines[l].length(); ++c){
+				if(lines[l].charAt(c) != '#' && lines[l].charAt(c) != ' ')
+					throw new ParseException("Invalid character '" +  lines[l].charAt(c) + "' in line " + l + ".", c);
+				blocks[l][c] = lines[l].charAt(c) == '#';
+			}
+		}
+		if(!color.matches("[0-9A-Fa-f]{6}?")){
+			throw new ParseException("Invalid color specification '" + color + "'.", 0);
+		}
+		this.color = color;
+	}
 
+	/**
+	 * Obtains this piece color.
+	 * @return This piece color.
+	 */
+	public String getColor() {
+		return color;
+	}
+	
+	/**
+	 * A simple factory method for pieces.
+	 * @param pieceSerialization The piece string representation.
+	 * @param color The color string, as hexa RRGGBB.
+	 * @return The new piece, or null if there is an error on parsing.
+	 */
+	public static Piece newInstance(String pieceSerialization, String color){
+		try {
+			return new Piece(pieceSerialization, color);
+		} catch (ParseException e) {
+			log.error("Fail to create piece.", e);
+		}
+		return null;
+	}
+	
 }
--- trunk/impl/Teamtris/src/net/sf/teamtris/piece/PieceManager.java
+++ trunk/impl/Teamtris/src/net/sf/teamtris/piece/PieceManager.java
@@ -1,5 +1,9 @@
 package net.sf.teamtris.piece;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * The entry point for the piece subsystem.
  * @author Leonardo
@@ -7,15 +11,24 @@ package net.sf.teamtris.piece;
  * @created 31-dez-2007 14:02:51
  */
 public class PieceManager {
+	private static final List<String> pieceTypes = Collections.unmodifiableList(Arrays.asList(new String[] {"Classic"}));
 
 	/**
 	 * Creates a new initialized piece stream with the given configuration.
 	 * @param type The stream type.
 	 * @param seed The stream seed for random pieces sequence.
 	 */
-	public static PieceStream createPieceStream(String type, int seed){
+	public static PieceStream createPieceStream(String type, long seed){
 		// TODO Implement this!
-		return new ClassicTetrisPieceStream(seed);
+		return new ConfigurableTetrisPieceStream(seed);
 	}
 
+	/**
+	 * Obtains all available piece stream types.
+	 * @return A list containing all available piece stream types.
+	 */
+	public static List<String> getPieceTypes(){
+		// TODO Implement this!
+		return pieceTypes;
+	}
 }
